6bf9eb8cb13672d2d3e9fef3f2dd7d6dd2c45e19
tyagi-parth5 <141475842+tyagi-parth5@users.noreply.github.com>
2024-11-18 12:55:05 +0530

    Add files via upload
    commit

---
+++ my file.R
@@ -0,0 +1,38 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
+}
+
+
+mat <- matrix(c(1, 2, 3, 4), 2, 2)
+cachedMatrix <- makeCacheMatrix(mat)
+print(cacheSolve(cachedMatrix))  # First time (calculates the inverse)
+cacheSolve(cachedMatrix)  # Second time (retrieves cached inverse)
+
